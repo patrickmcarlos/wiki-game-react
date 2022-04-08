ae51fec2f19ec485157eaf8325174401bc969408
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-04-08 01:16:26 -0700

    refactor: try setting base url directly into axios. remove console log

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-console.log(import.meta.env.VITE_BASE_API_URL);
+axios.defaults.baseURL = import.meta.env.VITE_BASE_API_URL as string;
 
 export const httpClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_API_URL as string,
