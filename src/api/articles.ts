8a69b986f1e91ac0954c4022f29d06caf70c36b0
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-04-08 01:28:35 -0700

    refactor: remove unnecessary base url when making request

--- src/api/articles.ts
+++ src/api/articles.ts
@@ -1,6 +1,6 @@
 import { httpClient } from './axios';
 
-const ARTICLES_ENDPOINT = `${import.meta.env.VITE_BASE_API_URL}/articles`;
+const ARTICLES_ENDPOINT = '/articles';
 
 export const getRandomArticles = async (numArticles: number) => {
     const params = new URLSearchParams([['num', String(numArticles)]]);
