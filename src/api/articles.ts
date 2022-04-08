5d0579b71d01e97211019af48cee5f7f11d93f86
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-04-08 01:35:19 -0700

    refactor: log base url for testing purposes

--- src/api/articles.ts
+++ src/api/articles.ts
@@ -4,7 +4,13 @@ const ARTICLES_ENDPOINT = '/articles';
 
 export const getRandomArticles = async (numArticles: number) => {
     const params = new URLSearchParams([['num', String(numArticles)]]);
-    const res = await httpClient.get(ARTICLES_ENDPOINT, { params });
+
+    console.log(import.meta.env.VITE_BASE_API_URL);
+    
+    const res = await httpClient.get(ARTICLES_ENDPOINT, {
+        params,
+        baseURL: import.meta.env.VITE_BASE_API_URL as string,
+    });
 
     return res.data;
 };
